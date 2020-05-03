7c0a0432e8ea3fbdfcc5a92be0a0250c4d6c4a8e
Dani M <dani_mihalache@yahoo.com>
2020-05-03 13:54:38 +0300

    FMCA-13 + FMCA-16 Created Login Window and Hiker Menu Design

--- src/main/java/com/grinningwalrus/jfx/JFX_APP.java
+++ src/main/java/com/grinningwalrus/jfx/JFX_APP.java
@@ -1,13 +1,26 @@
 package com.grinningwalrus.jfx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class JFX_APP extends Application {
-    public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(new Pane(), 800, 600));
-        stage.show();
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("sample.fxml")));
+        primaryStage.setTitle("Mountain Climbing Project");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
