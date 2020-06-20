b3be01eff16efafa408dd333e22fc2bca9131183
WindsorMammoth <razvan.miculit@gmail.com>
2020-06-20 15:46:55 +0300

    FMCA-32 minor touches

--- src/main/java/com/grinningwalrus/baseclasses/Record.java
+++ src/main/java/com/grinningwalrus/baseclasses/Record.java
@@ -2,24 +2,21 @@ package com.grinningwalrus.baseclasses;
 
 import java.util.ArrayList;
 
-public class Trip {
+public class Record {
     private String name;
     private String location;
-    private String date;
+    private String altitude;
 
-    private ArrayList<String> signed_up_users = new ArrayList<String>();
-
-    public Trip()
+    public Record()
     {
 
     }
 
-    public Trip(String name, String location, String date, ArrayList<String> signed_up_users)
+    public Record(String name, String location, String altitude)
     {
         this.name = name;
         this.location = location;
-        this.date = date;
-        this.signed_up_users = signed_up_users;
+        this.altitude = altitude;
     }
 
     public String getName() {
@@ -38,24 +35,16 @@ public class Trip {
         this.location = location;
     }
 
-    public String getDate() {
-        return date;
+    public String getAltitude() {
+        return altitude;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public ArrayList<String> getSigned_up_users() {
-        return signed_up_users;
-    }
-
-    public void setSigned_up_users(ArrayList<String> signed_up_users) {
-        this.signed_up_users = signed_up_users;
+    public void setAltitude(String altitude) {
+        this.altitude = altitude;
     }
 
     public String toString()
     {
-        return this.name + " - " + this.location + " - " + this.date;
+        return this.name + " - " + this.location + " - " + this.altitude;
     }
 }
